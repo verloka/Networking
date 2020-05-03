872f28fb33f5d10216ec912774db22a03d66d827
Vadym Verloka <verlokav@gmail.com>
2020-05-03 18:38:07 +0300

    Added the URITest and DNSTest projects.

---
+++ src/DNSTest/DNSTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace DNSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entry1 = Dns.GetHostEntry("verloka.com");
+            Console.WriteLine($"{entry1.HostName}:");
+            foreach (var ip in entry1.AddressList)
+                Console.WriteLine(ip);
+
+            var entry2 = Dns.GetHostEntry("1.1.1.1");
+            Console.WriteLine($"{entry2.HostName}:");
+            foreach (var ip in entry2.AddressList)
+                Console.WriteLine(ip);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+        }
+    }
+}
---
+++ src/URITest/URITest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace URITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVerlokaURI());
+            Console.WriteLine(GetVerlokaURICtor());
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+        }
+
+        static Uri GetVerlokaURI()
+        {
+            var builder = new UriBuilder
+            {
+                Scheme = "https",
+                Host = "verloka.com"
+            };
+
+            return builder.Uri;
+        }
+
+        static Uri GetVerlokaURICtor() => new UriBuilder("https", "verloka.com").Uri;
+    }
+}
