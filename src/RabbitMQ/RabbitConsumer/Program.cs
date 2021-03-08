c73821b59a5bf98c5551f77d56fbaa784f55d15b
Vadym Verloka <verlokav@gmail.com>
2021-03-08 13:11:47 +0200

    Added the RabbitMQ "Hello World" sample.

---
+++ src/RabbitMQ/RabbitConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += ConsumerReceived;
+
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+
+        private static void ConsumerReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received: {message}");
+        }
+    }
+}
---
+++ src/RabbitMQ/RabbitProducer/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RabbitMQ Produces.");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            while(true)
+            {
+                Console.WriteLine("To exit type 'exit'");
+                Console.Write("Send: ");
+                string str = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(str))
+                {
+                    Console.Clear();
+
+                    if (str != "exit")
+                    {
+                        var body = Encoding.UTF8.GetBytes(str);
+                        channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+
+                        Console.WriteLine($"Sended: {str}");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
